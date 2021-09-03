e33ac6f052c42476403ad16809ff94cdb2a5f868
aroyehunahmad <ahmadadewale800@gmail.com>
2021-09-03 06:52:04 +0100

    initial commit for part 7

--- src/index.js
+++ src/index.js
@@ -1,50 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import App from './App.js'
 
-const store = createStore(reducer)
+// import { createStore } from 'redux'
+// import reducer from './reducer'
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-  const ok = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-  const zero = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
+// const store = createStore(reducer)
 
-  return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button onClick={ok}>neutral</button> 
-      <button onClick={bad}>bad</button>
-      <button onClick={zero}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
-}
+// const App = () => {
+//   const good = () => {
+//     store.dispatch({
+//       type: 'GOOD'
+//     })
+//   }
+//   const ok = () => {
+//     store.dispatch({
+//       type: 'OK'
+//     })
+//   }
+//   const bad = () => {
+//     store.dispatch({
+//       type: 'BAD'
+//     })
+//   }
+//   const zero = () => {
+//     store.dispatch({
+//       type: 'ZERO'
+//     })
+//   }
 
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+//   return (
+//     <div>
+//       <button onClick={good}>good</button> 
+//       <button onClick={ok}>neutral</button> 
+//       <button onClick={bad}>bad</button>
+//       <button onClick={zero}>reset stats</button>
+//       <div>good {store.getState().good}</div>
+//       <div>neutral {store.getState().ok}</div>
+//       <div>bad {store.getState().bad}</div>
+//     </div>
+//   )
+// }
 
-renderApp()
-store.subscribe(renderApp)
-store.dispatch({type: 'GOOD'})
-console.log(store.getState().good)
+// const renderApp = () => {
+//   ReactDOM.render(<App />, document.getElementById('root'))
+// }
+
+// renderApp()
+// store.subscribe(renderApp)
+// store.dispatch({type: 'GOOD'})
+// console.log(store.getState().good)
+
+ReactDOM.render(<App />, document.getElementById('root'))
